02f86ccb380f1f55d1f0c7ad27d054a0c55fe7d9
Mikunryu <orkunatik@outlook.de>
2022-07-12 13:18:50 +0200

    Nochnmal spielen Funktion

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,16 @@ public class Main {
         Random rand = new Random();
         Scanner scanner = new Scanner(System.in); //Scanner Klasse um die Benutzer Eingabe möglich zu machen.
 
-        int zufallsZahl = rand.nextInt(100) + 1; //Hier wird die Zufallszahl generiert. 100 + 1, da dadurch eine Zahl zwischen 1-100 generiert wird.
+        int zufallsZahl; //Hier wird die Zufallszahl generiert. 100 + 1, da dadurch eine Zahl zwischen 1-100 generiert wird.
         //System.out.println("Die Zufallszahl ist " + zufallsZahl); //Cheat für Debugging
         System.out.println("Willkommen zum Zahlenraten Spiel!");
 
-
-            int Versuche = 0;
-            while (true) {
+        boolean nochmalSpielen = true;
+        int Versuche = 0;
+        while (nochmalSpielen) {
+            nochmalSpielen = false;
+            zufallsZahl = rand.nextInt(100) + 1;
+            while (Versuche < 5) {
                 System.out.println("Rate eine Zahl zwischen (1-100):");
 
                 int spielerEingabe = scanner.nextInt(); //Spieler Eingabe.
@@ -22,16 +25,28 @@ public class Main {
                 if (spielerEingabe == zufallsZahl) {
                     System.out.println("Die Antwort ist Richtig! Du hast gewonnen!");
                     System.out.println("Du hast " + Versuche + " Versuche gebraucht.");
-                    System.out.println("Würdest du gerne noch einmal Spielen J/N?");
-                    break; //Durch den break an dieser Stelle, endet das Programm, sobald man die Richtige Antwort erraten hat.
+                    Versuche = 5;
+                    //break; //Durch den break an dieser Stelle, endet das Programm, sobald man die Richtige Antwort erraten hat.
                 } else if (zufallsZahl > spielerEingabe) {
                     System.out.println("Leider Falsch! Die Zahl ist größer. Versuche es noch einmal.");
-                } else {
+                } else if (zufallsZahl < spielerEingabe) {
                     System.out.println("Leider Falsch! Die Zahl ist kleiner. Versuche es noch einmal.");
+                }
+            }
+                System.out.println("Willst du noch eine Runde spielen? (J/N)");
+                String erneutSpielen = scanner.nextLine();
+                if (erneutSpielen.equals("J")) {
+                    nochmalSpielen = true;
                 }
 
 
             }
+
         }
+
     }
+
+
+
+
 
